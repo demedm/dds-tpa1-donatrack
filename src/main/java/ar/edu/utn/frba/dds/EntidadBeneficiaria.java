c91e183b6a2cd5fd575549f92ebecc03c70008e0
demedm <denisemedinaam@gmail.com>
2026-05-21 15:15:31 -0300

    agrego tipo a entidad beneficiaria

--- src/main/java/ar/edu/utn/frba/dds/EntidadBeneficiaria.java
+++ src/main/java/ar/edu/utn/frba/dds/EntidadBeneficiaria.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public class EntidadBeneficiaria extends Entidad{
   String direccion;
+  String tipo;
 
-  public EntidadBeneficiaria(String razon, List<MedioContacto> telefonoOrganizacion, List<Mail> mailRepresentantes, String direccionEntidad){
+  public EntidadBeneficiaria(String razon, String tipoEntidad, List<MedioContacto> telefonoOrganizacion, List<Mail> mailRepresentantes, String direccionEntidad){
     super(razon, mailRepresentantes);
     SetMedioContacto(telefonoOrganizacion);
     direccion = direccionEntidad;
+    tipo = tipoEntidad; //escuelas rurales, comedores, espacios de tutoría de niños, entre otros
   }
 
 }
